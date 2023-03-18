a254d4524ae4430120816a4349714e82dbdcdb58
Mykola Rostotskyi <1nick.rost@gmail.com>
2023-03-18 15:14:09 +0200

    Add project files.

---
+++ CSGOWingmanMatchHistoryViewer/Models/GetNextMatchResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGOWingmanMatchHistoryViewer.Models
+{
+    internal class GetNextMatchResponse
+    {
+        public bool Success { get; set; }
+        public string Html { get; set; }
+        public string ContinueToken { get; set; }
+        public string ContinueText { get; set; }
+
+    }
+}
---
+++ CSGOWingmanMatchHistoryViewer/Services/MatchService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGOWingmanMatchHistoryViewer.Services
+{
+    public class MatchService
+    {
+        private readonly HttpClient _httpClient;
+        public MatchService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetMatchDetails(string matchToken, string cookiesHeader, string steamId)
+        {
+            _httpClient.BaseAddress = new Uri("https://steamcommunity.com/profiles/76561198141127669/gcpd/730");
+            _httpClient.DefaultRequestHeaders.Add("Cookie", cookiesHeader);
+            _httpClient.DefaultRequestHeaders.Add("Referer", $"https://steamcommunity.com/profiles/{steamId}/gcpd/730/?tab=matchhistorywingman");
+
+            await _httpClient.GetAsync(matchToken);
+
+            return "";
+        }
+    }
+}
